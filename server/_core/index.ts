5c3befe7a445a9088decacfd4d5307afb19df20f
MahmoudQawry <mahmoudqawry@gmail.com>
2026-09-22 03:49:35 +0400

    hardening: reduce global HTTP payload limits

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -54,8 +54,8 @@ async function startServer() {
     next();
   });
 
-  app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({ limit: "50mb", extended: true }));
+  app.use(express.json({ limit: "2mb" }));
+  app.use(express.urlencoded({ limit: "2mb", extended: true }));
 
   registerStorageProxy(app);
   registerOAuthRoutes(app);
