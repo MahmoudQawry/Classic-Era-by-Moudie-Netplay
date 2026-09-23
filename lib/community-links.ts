5aad2ebd079309ced5d0cf6001a29352c6f1ce67
MahmoudQawry <mahmoudqawry@gmail.com>
2026-09-24 02:18:13 +0400

    feat: add official community links

---
+++ lib/community-links.ts
@@ -0,0 +1,6 @@
+export const COMMUNITY_LINKS = {
+  discord: "https://discord.gg/9KtKFVH5m",
+  discordLabel: "discord.gg/9KtKFVH5m",
+  telegram: "https://t.me/ClassicEraByMoudieNetplay",
+  telegramLabel: "t.me/ClassicEraByMoudieNetplay",
+} as const;
