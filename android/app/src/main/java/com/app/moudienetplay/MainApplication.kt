fdad30d1d5ff81ae55bd4a517e71f6c63827027c
MahmoudQawry <mahmoudqawry@gmail.com>
2026-09-24 12:00:22 +0400

    feat: wire Discord Social SDK into Android host

--- android/app/src/main/java/com/app/moudienetplay/MainApplication.kt
+++ android/app/src/main/java/com/app/moudienetplay/MainApplication.kt
@@ -28,8 +28,8 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              // Packages that cannot be autolinked yet can be added manually here.
+              add(DiscordSocialPackage())
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
