b7181d56e7475e9c1fe96b63bcb497b089e18350
MahmoudQawry <mahmoudqawry@gmail.com>
2026-09-24 12:00:45 +0400

    feat: add Expo config plugin for Discord SDK callback

---
+++ plugins/with-discord-social-sdk.js
@@ -0,0 +1,37 @@
+const { withAndroidManifest } = require("@expo/config-plugins");
+
+module.exports = function withDiscordSocialSdk(config) {
+  return withAndroidManifest(config, (mod) => {
+    const applicationId = process.env.DISCORD_APPLICATION_ID?.trim();
+    if (!applicationId) return mod;
+
+    const mainApplication = mod.modResults.manifest.application?.[0];
+    if (!mainApplication) return mod;
+
+    const activities = mainApplication.activity ?? [];
+    const exists = activities.some(
+      (activity) => activity.$?.["android:name"] === "com.discord.socialsdk.AuthenticationActivity",
+    );
+    if (!exists) {
+      activities.push({
+        $: {
+          "android:name": "com.discord.socialsdk.AuthenticationActivity",
+          "android:exported": "true",
+        },
+        "intent-filter": [
+          {
+            action: [{ $: { "android:name": "android.intent.action.VIEW" } }],
+            category: [
+              { $: { "android:name": "android.intent.category.DEFAULT" } },
+              { $: { "android:name": "android.intent.category.BROWSABLE" } },
+            ],
+            data: [{ $: { "android:scheme": `discord-${applicationId}` } }],
+          },
+        ],
+      });
+    }
+
+    mainApplication.activity = activities;
+    return mod;
+  });
+};
